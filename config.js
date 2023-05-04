096579183d77e7121efe47c7796cac6f29b4e227
jolieleung <102486311+jolieleung@users.noreply.github.com>
2023-05-04 00:41:10 -0400

    narrative progress, styling, rasters in mapbox

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var config = {
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
-            id: 'landing-1', //ids need to be unique for each chapter
+            id: 'overall-utcci-1', //ids need to be unique for each chapter
             alignment: 'left',
             hidden: false,
             title: 'Urban Tree Cover Change',
@@ -51,12 +51,12 @@ var config = {
             ]
         },
         {
-            id: 'nys_utcci_3d', //ids need to be unique for each chapter
+            id: 'overall-tci-1', //ids need to be unique for each chapter
             alignment: 'left',
             hidden: false,
-            title: 'Urban Tree Cover Change',
+            title: 'Temperature Change Index',
             image: '',
-            description: 'The dataset also has implications that vulnerable communities in urban areas, particularly those with lower socio-economic status, are disproportionately affected by the loss of tree cover, exacerbating existing environmental and health disparities. These findings underscore the urgent need for effective urban forest management strategies and climate adaptation measures to mitigate the impacts of climate change on urban tree cover and enhance the resilience of urban ecosystems in the counterminous United States.',
+            description: 'We are able to see that temperature is steadier along the coasts, and most of the riskier temperature change index is inland, with the riskiest areas located in the midwest.', 
             location: {
                 center: [-97.86118, 40.54797],
                 zoom: 3.95,
@@ -73,16 +73,251 @@ var config = {
                      duration: 5000
                  },
                  {
-                    layer: 'utcci_3d',
+                    layer: 'tci',
                     opacity: 1,
                     duration: 5000
                 }
             ],
             onChapterExit: [
                  {
-                     layer: 'utcci_3d',
+                     layer: 'tci',
+                     opacity: 1
+                 }
+            ]
+        },
+        {
+            id: 'overall-tci-2', //ids need to be unique for each chapter
+            alignment: 'left',
+            hidden: false,
+            title: 'Plant Hardiness Zones to Measure Temperature',
+            image: './images/phz_90-99.gif',
+            description: 'Based on the dataset from the U.S. Forest Service, the findings reveal significant changes in plant hardiness zones in the conterminous United States. The data indicates that plant hardiness zones, which provide information on the average annual minimum temperatures for different regions, have shifted northward and upward in elevation over time, reflecting the impact of climate change. Warmer minimum temperatures have resulted in the expansion of plant hardiness zones towards higher latitudes and altitudes, allowing for the potential migration of plant species to new areas. However, the dataset also shows that these changes are not uniform across the country, with variations in the magnitude and direction of the shifts depending on geographic location. These shifts in plant hardiness zones have important implications for plant distribution, ecosystem dynamics, and agriculture, and highlight the need for ongoing monitoring and adaptive management to mitigate the impacts of changing climate on plant communities in the United States.',
+            location: {
+                center: [-97.86118, 40.54797],
+                zoom: 3.95,
+                pitch: 54,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                 {
+                     layer: 'state-boundary',
+                     opacity: 1,
+                     duration: 5000
+                 },
+                 {
+                    layer: 'tci',
+                    opacity: 1,
+                    duration: 5000
+                }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'tci',
+                     opacity: 1
+                 }
+            ]
+        },
+        {
+            id: 'overall-tci-3', //ids need to be unique for each chapter
+            alignment: 'left',
+            hidden: false,
+            title: 'Temperature Scatterplot',
+            image: '',
+            description: 'Below is a scatterplot comparing the mean Urban Tree Cover Change Index with the Temperature Change Index per state. The dashed line represents the country mean for both metrics. The values closest to 0, marked by a light blue line, represent no change.' + 
+            '<br/><div class="video-container"><iframe src="./plots/tci_scatter.html" frameborder="0" scrolling="no" /></div>',
+            location: {
+                center: [-97.86118, 40.54797],
+                zoom: 3.95,
+                pitch: 54,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                 {
+                     layer: 'state-boundary',
+                     opacity: 1,
+                     duration: 5000
+                 },
+                 {
+                    layer: 'tci',
+                    opacity: 1,
+                    duration: 5000
+                }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'tci',
                      opacity: 0
                  }
+            ]
+        },            
+        {
+            id: 'overall-apci', //ids need to be unique for each chapter
+            alignment: 'left',
+            hidden: false,
+            title: 'Absolute Precipitation Change Index',
+            image: '',
+            description: 'We are able to see that throughout the eastern side of the country, there is higher risk for preciptation change. Though this metric does not account for whether the change is an increase or a decrease in precipitation. Below is a scatterplot comparing the mean Urban Tree Cover Change Index with the Absolute Preciptation Change Index per state. The dashed line represents the country mean for both metrics. The values closest to 0, marked by a light blue line, represent no change.' + 
+            '<br/><div class="video-container"><iframe src="./plots/apci_scatter.html" frameborder="0" scrolling="no" /></div>',
+            location: {
+                center: [-97.86118, 40.54797],
+                zoom: 3.95,
+                pitch: 54,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                 {
+                     layer: 'state-boundary',
+                     opacity: 1,
+                     duration: 5000
+                 },
+                 {
+                    layer: 'apci',
+                    opacity: 1,
+                    duration: 5000
+                }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'apci',
+                     opacity: 0
+                 }
+            ]
+        },
+        {
+            id: 'overall-aci', //ids need to be unique for each chapter
+            alignment: 'left',
+            hidden: false,
+            title: 'Aridity Change Index',
+            image: '',
+            description: 'We are able to get more clarity in which areas will get less rain with this metric. We see that throughout the eastern side of the country, there is higher risk for aridity change, with the exception of the central eastern coastal states. Below is a scatterplot comparing the mean Urban Tree Cover Change Index with the Arridity Change Index per state. The dashed line represents the country mean for both metrics. The values closest to 0, marked by a light blue line, represent no change.' + 
+            '<br/><div class="video-container"><iframe src="./plots/aci_scatter.html" frameborder="0" scrolling="no" /></div>',
+            location: {
+                center: [-97.86118, 40.54797],
+                zoom: 3.95,
+                pitch: 54,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                 {
+                     layer: 'state-boundary',
+                     opacity: 1,
+                     duration: 5000
+                 },
+                 {
+                    layer: 'aci',
+                    opacity: 1,
+                    duration: 5000
+                }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'aci',
+                     opacity: 0
+                 }
+            ]
+        },
+        {
+            id: 'overall_utcci-2', //ids need to be unique for each chapter
+            alignment: 'left',
+            hidden: false,
+            title: 'Urban Tree Cover Change',
+            image: '',
+            description: 'These three factors, temperature, precipitation, and arridity all have an effect on tree communities and tree cover. When visualizing the urban tree cover indexes, we can see most of the country is at risk for urban tree cover change, though the metric does not specify whether it is an increase or a decrease in tree cover. However, when ecosystems are exposed to drasically different climates, for the species that cannot adapt, the implications are usually that that change is reduction.',
+            location: {
+                center: [-97.86118, 40.54797],
+                zoom: 3.95,
+                pitch: 54,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                 {
+                     layer: 'state-boundary',
+                     opacity: 1,
+                     duration: 5000
+                 },
+                 {
+                    layer: 'utcci',
+                    opacity: 1,
+                    duration: 5000
+                }
+            ],
+            onChapterExit: [
+                 {
+                     layer: 'utcci',
+                     opacity: 0
+                 }
+            ]
+        },
+        {
+            id: 'nys_treecover',
+            alignment: 'right',
+            hidden: false,
+            title: 'Temperature Change Index',
+            image: '',
+            description: '',
+            location: {
+                center: [-75.45871, 42.87633],
+                zoom: 6.98,
+                pitch: 45.00,
+                bearing: 0.00,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {
+                    layer: 'TCC_Risk2',
+                    opacity: 1,
+                    duration: 5000
+                },
+                {
+                    layer: 'TCC_Risk3',
+                    opacity: 1,
+                    duration: 5000
+                },
+                {
+                    layer: 'TCC_Risk4',
+                    opacity: 1,
+                    duration: 5000
+                },
+                {
+                    layer: 'TCC_Risk5',
+                    opacity: 1,
+                    duration: 5000
+                },
+                {
+                    layer: 'county',
+                    opacity: 1,
+                    duration: 5000
+                },
+            ],
+            onChapterExit: [
+                {
+                    layer: 'TCC_Risk5',
+                    opacity: 0,
+                    duration: 5000
+                }
             ]
         },
         {
@@ -91,7 +326,7 @@ var config = {
             hidden: false,
             title: 'Temperature Change Index',
             image: './images/phz_90-99.gif',
-            description: 'Based on the dataset from the U.S. Forest Service, the findings reveal significant changes in plant hardiness zones in the conterminous United States. The data indicates that plant hardiness zones, which provide information on the average annual minimum temperatures for different regions, have shifted northward and upward in elevation over time, reflecting the impact of climate change. Warmer minimum temperatures have resulted in the expansion of plant hardiness zones towards higher latitudes and altitudes, allowing for the potential migration of plant species to new areas. However, the dataset also shows that these changes are not uniform across the country, with variations in the magnitude and direction of the shifts depending on geographic location. These shifts in plant hardiness zones have important implications for plant distribution, ecosystem dynamics, and agriculture, and highlight the need for ongoing monitoring and adaptive management to mitigate the impacts of changing climate on plant communities in the United States.',
+            description: '',
             location: {
                 center: [-75.45871, 42.87633],
                 zoom: 6.98,
@@ -114,7 +349,7 @@ var config = {
                 //     duration: 5000
                 // },
                 {
-                    layer: 'tci_3d',
+                    layer: 'tci',
                     opacity: 1,
                     duration: 5000
                 }
@@ -126,7 +361,7 @@ var config = {
                 //     duration: 5000
                 // },
                 {
-                    layer: 'tci_3d',
+                    layer: 'tci',
                     opacity: 0,
                     duration: 5000
                 }
@@ -150,14 +385,14 @@ var config = {
             callback: '',
             onChapterEnter: [
                 {
-                    layer: 'apci_3d',
+                    layer: 'apci',
                     opacity: 1,
                     duration: 5000
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'apci_3d',
+                    layer: 'apci',
                     opacity: 0,
                     duration: 5000
                 }
@@ -181,14 +416,14 @@ var config = {
             callback: '',
             onChapterEnter: [
                 {
-                    layer: 'aci_3d',
+                    layer: 'aci',
                     opacity: 1,
                     duration: 5000
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'aci_3d',
+                    layer: 'aci',
                     opacity: 0,
                     duration: 5000
                 }
@@ -212,14 +447,14 @@ var config = {
             callback: '',
             onChapterEnter: [
                 {
-                    layer: 'utcci_3d',
+                    layer: 'utcci',
                     opacity: 1,
                     duration: 5000
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'utcci_3d',
+                    layer: 'utcci',
                     opacity: 0,
                     duration: 5000
                 }
